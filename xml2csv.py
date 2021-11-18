f8a03254b6463f80e820d178ca3131bfe279fa3a
Brian Davis <brian.davis@veeva.com>
2021-11-17 20:16:30 -0500

    use glob to find strings.xml only from subdirectories

--- xml2csv.py
+++ xml2csv.py
@@ -6,16 +6,21 @@ from xml.dom import minidom
 import csv
 import os
 import sys
+import glob
 
 directory_with_android_resources = sys.argv[1]
-xml_files = os.listdir(directory_with_android_resources)
+
+# xml_files = os.listdir(directory_with_android_resources)
+search_dir = directory_with_android_resources + "/**/strings.xml"
+xml_files = glob.glob(search_dir, recursive=True)
+
 xml_parsed_files = []
 translations = []
 translations_dictionary = {}
 
 for xml_file in xml_files:
     resources_xml_file = directory_with_android_resources + "/" + xml_file
-    xml_parsed_files.append(minidom.parse(resources_xml_file).getElementsByTagName('string'))
+    xml_parsed_files.append(minidom.parse(xml_file).getElementsByTagName('string'))
 
 for xml_parsed_file in xml_parsed_files:
     for translation_key in xml_parsed_file:
@@ -36,14 +41,14 @@ writer = csv.writer(open("translations.csv", "w"), delimiter='\t', quotechar='',
 writer.writerow(['key'] + xml_files)
 # Sort one values, such as translatable="false" elements
 for translation_key in sorted(translations_dictionary, key=lambda k: len(translations_dictionary[k]), reverse=True):
-    print "\ntranslation_key: " + translation_key
+    print("\ntranslation_key: " + translation_key)
 
     translation_values = []
     for translation in translations_dictionary[translation_key]:
-        entersRemoved = " ".join(translation.encode('utf-8').split("\n"))
+        entersRemoved = " ".join(translation.split('\n'))
         multiSpacesRemoved = " ".join(entersRemoved.split())
         tabReplaced = "\\t".join(multiSpacesRemoved.split("\t"))
         translation_values.append(tabReplaced)
-        print "translation : " + tabReplaced
+        print("translation : " + tabReplaced)
 
     writer.writerow([translation_key] + translation_values)
